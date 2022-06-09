f573fba198cae81edf1c60a8861b2051cf777664
smoothstill <59178987+smoothstill@users.noreply.github.com>
2022-06-09 20:39:48 +0300

    fuel canister added

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,9 +5,11 @@ signal killed()
 
 var max_vertical_speed := 10000
 var min_vertical_speed := -10000
+var max_horizontal_speed := 300
+var horizontal_acceleration:= 800
+var jetpack_acceleration := 9
 var vertical_speed := 0.0
-var horizontal_speed := 300
-var jetpack_speed := 9
+var horizontal_speed := 0.0
 var gravity := 5
 export var max_fuel := 100.0
 onready var fuel_amount = max_fuel setget _set_fuel
@@ -30,7 +32,7 @@ func _process(delta):
 func _vertical_movement(delta):
 	var localUp = Vector2.UP
 	if Input.is_action_pressed("jump"):
-		vertical_speed += delta * jetpack_speed
+		vertical_speed += delta * jetpack_acceleration
 		_set_fuel(fuel_amount - fuel_decrease_speed)
 	vertical_speed -= delta * gravity # Gravity
 	if vertical_speed < min_vertical_speed:
@@ -39,9 +41,13 @@ func _vertical_movement(delta):
 		vertical_speed = max_vertical_speed
 	var k = move_and_collide(localUp * vertical_speed)
 	if k != null:
+		print(k.normal)
 		vertical_speed = 0
 
 # Checks WASD and Shift for horizontal movement via move_and_slide.
 func _horizontal_movement(delta):
 	var localRight = Vector2.RIGHT
+	horizontal_speed += delta * horizontal_acceleration
+	if (horizontal_speed > max_horizontal_speed):
+		horizontal_speed = max_horizontal_speed
 	move_and_slide(localRight * horizontal_speed)
---
+++ scripts/fuel_canister.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var fuel_supply = 20
+
+func _on_FuelCanister_body_entered(body: KinematicBody2D):
+	if (body.name == "Player"):
+		body._set_fuel(body.fuel_amount + fuel_supply)
+		queue_free()
